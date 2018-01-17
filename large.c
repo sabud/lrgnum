47082eecfa1026e64a46b96ddafbe1273cd0a5f2
sabud <35525196+sabud@users.noreply.github.com>
2018-01-17 16:25:16 +0530

    Update large.c

--- large.c
+++ large.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 #include<conio.h>
-void main()
+int main()
 {
 int num1, num2, num3;
 clrscr();
-printf("Enter the values of num1, num2 and num3\n");
+printf("Enter the values of num1, num2 and num3");
 scanf("%d %d %d", &num1, &num2, &num3);
 printf("num1 = %d\tnum2 = %d\tnum3 = %d\n", num1, num2, num3);
 if (num1 > num2)
 {
 if (num1 > num3)
 {
-printf("num1 is the greatest among three \n");
+printf("num1 is the greatest among three ");
 }
 else
 {
-printf("num3 is the greatest among three \n");
+printf("num3 is the greatest among three ");
 }
 }
 else if (num2 > num3)
-printf("num2 is the greatest among three \n");
+printf("num2 is the greatest among three ");
 else
-printf("num3 is the greatest among three \n");
+printf("num3 is the greatest among three ");
 getch();
 }
